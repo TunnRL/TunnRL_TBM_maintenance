fbb356d2bc0329a0c74fa5f3d248917fe1b8d535
G. H. Erharter <32653899+geograz@users.noreply.github.com>
2022-07-28 09:51:48 +0200

    Minor updates and addition of parameters
    Only minor updates on these codes and the addition of some parameters to the "hyperparams" file.

--- src/A_main.py
+++ src/A_main.py
@@ -15,7 +15,7 @@ import joblib
 import numpy as np
 import optuna
 from pathlib import Path
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, DDPG, TD3
 from stable_baselines3.common.env_checker import check_env
 import warnings
 
@@ -34,7 +34,7 @@ LIFE = 400000  # theoretical durability of one cutter [m]
 STROKE_LENGTH = 1.8  # length of one stroke [m]
 MAX_STROKES = 1000  # number of strokes per episode
 
-EPISODES = 50_000  # max episodes to train for
+EPISODES = 10_000  # max episodes to train for
 # evaluations in optimization and checkpoints in training every X episodes
 CHECKPOINT = 100
 
@@ -43,11 +43,12 @@ t_C_max = 75  # maximum time to change one cutter [min]
 # MODE determines if either an optimization should run = "Optimization", or a
 # new agent is trained with prev. optimized parameters = "Training", or an
 # already trained agent is executed = "Execution"
-MODE = 'Training'  # 'Optimization', 'Training', 'Execution'
+MODE = 'Execution'  # 'Optimization', 'Training', 'Execution'
+N_DEFAULT_TRIALS = 0  # n trials with default parameters to insert in study
 # name of the study if MODE == 'Optimization' or 'Training'
 # the Study name must start with the name of the agent that needs to be one of
 # 'PPO', 'A2C', 'DDPG', 'SAC', 'TD3'
-STUDY = 'PPO_2022_07_25_study'  # SAC_2022_07_23_study PPO_2022_07_23_study
+STUDY = 'DDPG_2022_07_27_study'  # DDPG_2022_07_27_study
 
 ###############################################################################
 # computed variables and instantiations
@@ -82,11 +83,10 @@ if MODE == 'Optimization':  # study
         print('new optimization study created')
     # the OPTUNA study is then run in a loop so that intermediate results are
     # saved and can be checked every trials
-    study = optim.enqueue_defaults(study, agent, n_trials=0)
+    study = optim.enqueue_defaults(study, agent, n_trials=N_DEFAULT_TRIALS)
     for _ in range(200):  # save every second study
         study.optimize(optim.objective, n_trials=1, catch=(ValueError,))
         joblib.dump(study, Path(f'results/{STUDY}.pkl'))
-
         df = study.trials_dataframe()
         df.to_csv(Path(f'results/{STUDY}.csv'))
 
@@ -102,10 +102,10 @@ elif MODE == 'Training':
     optim.objective(study.best_trial)
 
 elif MODE == 'Execution':
-    model = 'PPO20220722-142740'  # name of the model to load
+    model = 'DDPG20220728-092315'  # name of the model to load
     tests = 3  # number of test episodes
 
-    agent = PPO.load(Path(f'optimization\{model}\best_model'))  # fr'checkpoints\{model}')
+    agent = DDPG.load(Path(fr'checkpoints\{model}\best_model'))
 
     # test agent throughout multiple episodes
     for test in range(tests):
@@ -124,7 +124,7 @@ elif MODE == 'Execution':
             # collect number of broken cutters in curr. state
             broken_cutters.append(len(np.where(state == 0)[0]))
             # agent takes an action -> tells which cutters to replace
-            action = agent.predict(state, deterministic=True)[0]
+            action = agent.predict(state, deterministic=False)[0]
             # environment gives new state signal, terminal flag and reward
             state, reward, terminal, info = env.step(action)
             # collect actions, states and rewards for later analyses
--- src/XX_hyperparams.py
+++ src/XX_hyperparams.py
@@ -32,3 +32,33 @@ class parameters:
                              'SAC_target_update_interval': 1,
                              'SAC_use sde': False,
                              'SAC_use_sde_at_warmup': False}
+
+        self.A2C_defaults = {'A2C_learning rate': 0.0007,
+                             'A2C_n steps': 5,
+                             'A2C_discount': 0.99,
+                             'A2C_gae lambda': 1.0,
+                             'A2C_ent_coef': 0.0,
+                             'A2C_vf coef': 0.5,
+                             'A2C_max grad norm': 0.5,
+                             'A2C_rms_prop_eps': 1e-05}
+
+        self.DDPG_defaults = {'DDPG_learning rate': 0.001,
+                              'DDPG_batch_size': 100,
+                              'DDPG_learning starts': 100,
+                              'DDPG_tau': 0.005,
+                              'DDPG_discount': 0.99,
+                              # 'DDPG_train_freq': (1, 'episode'),
+                              'DDPG_gradient_steps': -1}
+
+        self.TD3_defaults = {'TD3_learning rate': 0.001,
+                             'TD3_learning starts': 100,
+                             'TD3_batch_size': 100,
+                             'TD3_tau': 0.005,
+                             'TD3_discount': 0.99,
+                             # 'TD3_train_freq': (1, 'episode'),
+                             'TD3_gradient_steps': -1,
+                             'TD3_action_noise': None,
+                             'TD3_policy_delay': 2,
+                             'TD3_target_policy_noise': 0.2,
+                             'TD3_target_noise_clip': 0.5}
+
