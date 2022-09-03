6969a478ea22c556a4866de1b1cb974fa0c47af0
tfha <tom.frode.hansen@gmail.com>
2022-09-03 21:30:12 +0200

    implementing the recommender function with a proper cli

--- src/E_recommender.py
+++ src/E_recommender.py
@@ -10,6 +10,7 @@ code contributors: Tom F. Hansen
 """
 import uuid
 
+import click
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -18,44 +19,65 @@ from numpy.typing import NDArray
 from XX_experiment_factory import load_best_model
 
 
-def recommend(
-    data_path: str,
-    agent_name: str,
-    agent_main_dir: str,
-    agent_dir: str,
-    save_path: str,
-) -> None:
+@click.command()
+@click.option("--n_c_tot", default=28, type=int, help="Number of cutters on cutterhead.")
+@click.option(
+    "-dp",
+    "--data_path",
+    default="results/cutter_states.csv",
+    type=click.Path(exists=True, readable=True, dir_okay=True),
+    help="Path to data file with TBM cutter life data",
+)
+@click.option(
+    "-a",
+    "--agent_name",
+    default="PPO",
+    type=click.Choice(["PPO", "DDPG", "A2C", "TD3", "SAC"], case_sensitive=False),
+)
+@click.option(
+    "-ap",
+    "--agent_path",
+    default="checkpoints/PPO_f7169560-3771-44b6-95f0-8456e45e23f6",
+    type=click.Path(exists=True),
+    help="Path to trained agent",
+)
+@click.option(
+    "-sd",
+    "--save_dir",
+    default="graphics",
+    type=click.Path(exists=True),
+    help="Dir to save heatmap of actions",
+)
+def recommend(n_c_tot, data_path, agent_name, agent_path, save_dir) -> NDArray:
     """Reading a state vector for the TBM-cutters from a csv-file and
-    recommend the next actions."""
+    recommend the next actions.
+    Saves a heatmap. Returns an array of actions"""
     observation = pd.read_csv(data_path, header=None).values[0]
     broken_cutters = np.where(observation == 0)[0]
     good_cutters = np.where(observation > 0)[0]
 
     print("Cutter statistics:")
     print("Observation: ", observation)
-    print("Good cutters: ", good_cutters)
-    print("Broken cutters: ", broken_cutters)
+    print("Good cutters: ", len(good_cutters))
+    print("Broken cutters: ", len(broken_cutters))
+
+    main_dir = "/".join(agent_path.split("/")[0:-1])
+    agent_dir = agent_path.split("/")[-1]
 
     agent = load_best_model(
-        agent_name=agent_name, main_dir=agent_main_dir, agent_dir=agent_dir
+        agent_name=agent_name, main_dir=main_dir, agent_dir=agent_dir
     )
     actions = agent.predict(observation, deterministic=False)[0]
 
-    ac_reshaped = actions.reshape((N_C_TOT, N_C_TOT))
+    ac_reshaped = actions.reshape((n_c_tot, n_c_tot))
     fig, ax = plt.subplots()
     ax.imshow(ac_reshaped)
+    save_path = f"{save_dir}/recommend_actions_{uuid.uuid4()}.svg"
     plt.savefig(save_path)
 
     return actions
 
 
 if __name__ == "__main__":
-    MAIN_DIR = "checkpoints"  # 'checkpoints' 'optimization'
-    AGENT_DIR = "PPO_f7169560-3771-44b6-95f0-8456e45e23f6"
-    SAVEPATH = f"graphics/recommend_actions_{uuid.uuid4()}.svg"
-    DATAPATH = "results/cutter_states.csv"
-    N_C_TOT = 28
-    agent_name = AGENT_DIR.split("_")[0]  # 'PPO' 'A2C' 'DDPG' 'TD3' 'SAC'
-
-    actions = recommend(DATAPATH, agent_name, MAIN_DIR, AGENT_DIR, SAVEPATH)
+    actions = recommend()
     print(actions)
