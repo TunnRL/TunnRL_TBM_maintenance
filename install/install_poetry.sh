3e623438a6e56ffba132212ea63ba90211526a75
tfha <tom.frode.hansen@gmail.com>
2022-11-24 10:05:17 +0100

    update lock file + some cleaning

--- install/install_poetry.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# installs the poetry packaging system into your system
-curl -sSL https://install.python-poetry.org | python3 -
-
-# add poetry to path
-echo 'export PATH="$HOME/.poetry/bin:$PATH"' >> ~/.bash_aliases
